706bd30d8a181ee883c26bf4efa6264311add353
Patriarch <jdackdack@gmail.com>
2021-05-14 23:11:52 +0300

    deleted GenericGame

--- kursach2/eidos/games/models.py
+++ kursach2/eidos/games/models.py
@@ -3,20 +3,25 @@ from django.db import models
 class Games(models.Model):
     pass
 
-class GenericStoreGames(models.Model):
+
+
+class ZakaGames(models.Model):
     game_name = models.CharField(primary_key=True, max_length=50)
     game_url = models.URLField()
     price_amount = models.FloatField()
     price_currency = models.CharField(max_length=10)
 
-class ZakaGames(GenericStoreGames):
-    pass
+class GogGames(models.Model):
+    game_name = models.CharField(primary_key=True, max_length=50)
+    game_url = models.URLField()
+    price_amount = models.FloatField()
+    price_currency = models.CharField(max_length=10)
 
-class GogGames(GenericStoreGames):
-    pass
-
-class SteamPayGames(GenericStoreGames):
-    pass
+class SteamPayGames(models.Model):
+    game_name = models.CharField(primary_key=True, max_length=50)
+    game_url = models.URLField()
+    price_amount = models.FloatField()
+    price_currency = models.CharField(max_length=10)
 
 class Articles(models.Model):
     title = models.CharField('Title', max_length=50)
