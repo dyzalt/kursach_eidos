31395218b285193ecf700d106440e67e2eddcb90
Patriarch <jdackdack@gmail.com>
2021-05-14 22:40:45 +0300

    Added GenericStoreGames

--- kursach2/eidos/games/models.py
+++ kursach2/eidos/games/models.py
@@ -1,5 +1,23 @@
 from django.db import models
 
+class Games(models.Model):
+    pass
+
+class GenericStoreGames(models.Model):
+    game_name = models.CharField(primary_key=True, max_length=50)
+    game_url = models.URLField()
+    price_amount = models.FloatField()
+    price_currency = models.CharField(max_length=10)
+
+class ZakaGames(GenericStoreGames):
+    pass
+
+class GogGames(GenericStoreGames):
+    pass
+
+class SteamPayGames(GenericStoreGames):
+    pass
+
 class Articles(models.Model):
     title = models.CharField('Title', max_length=50)
     text = models.TextField('Text')
